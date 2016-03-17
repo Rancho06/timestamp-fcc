62daa4073c6b429ee59efcf958bb7eece0c5b3c0
Shu Zhou <shuzhou@usc.edu>
2016-03-17 15:35:38 -0400

    updated port

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const express = require('express');
 const routes = require('./route');
 
 const app = express();
+app.set("port", process.env.PORT || 8080);
    
 routes(app);
 
-app.listen(8080, function () {
-   console.log('Node.js listening on port 8080...');
+app.listen(app.get("port"), function () {
+   console.log(`Node.js listening on port ${app.get("port")}...`);
 });
 
